0e4a7a8baed2484625b7ab236affd53639433a22
Ben Giacalone <bsg8294@cs.rit.edu>
2023-09-21 17:28:57 -0400

    Run stopword experiment

--- stopword_removal.py
+++ stopword_removal.py
@@ -53,13 +53,12 @@ topic, qrels = process_ds()
 Q = "[unused0]"
 prune_tokens = [Q, "[SEP]", "[MASK]", "[CLS]"]
 prune_set = set(tokenizer.convert_tokens_to_ids(prune_tokens))
-prune_tokens_str = prune_tokens
 if args.no_pruning:
     pass
 else:
     tokenized_ids: list = list(set(sum(tokenizer(stopwords.words("english"), add_special_tokens=False).input_ids, [])))
     prune_set = prune_set.union(set(tokenized_ids))
-    prune_tokens_str += tokenizer.convert_ids_to_tokens(list(prune_set))
+prune_tokens_str = tokenizer.convert_ids_to_tokens(list(prune_set))
 
 dense_e2e_bert_pruned = pytcolbert.end_to_end(prune_set, prune_queries=True, prune_documents=False)
 print(f"Experiment: Tokens to prune: {prune_tokens_str}")
