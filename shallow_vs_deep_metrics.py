c495083f63fcbb758bc150fd8e4527435e1ac1ce
Ben Giacalone <metakirby123@gmail.com>
2023-09-28 13:00:29 -0400

    Add shallow vs deep metrics experiment

--- shallow_vs_deep_metrics.py
+++ shallow_vs_deep_metrics.py
@@ -51,13 +51,11 @@ topic, qrels = process_ds()
 
 # Token IDs.
 Q = "[unused0]"
-prune_tokens = [Q, "[SEP]", "[MASK]", "[CLS]"]
-prune_set = set(tokenizer.convert_tokens_to_ids(prune_tokens))
 if args.no_pruning:
-    pass
+    prune_set = set()
 else:
-    tokenized_ids: list = list(set(sum(tokenizer(stopwords.words("english"), add_special_tokens=False).input_ids, [])))
-    prune_set = prune_set.union(set(tokenized_ids))
+    prune_tokens = [Q, "[SEP]", "[MASK]", "[CLS]"]
+    prune_set = set(tokenizer.convert_tokens_to_ids(prune_tokens))
 prune_tokens_str = tokenizer.convert_ids_to_tokens(list(prune_set))
 
 dense_e2e_bert_pruned = pytcolbert.end_to_end(prune_set, prune_queries=True, prune_documents=False)
@@ -72,7 +70,7 @@ pt.Experiment(
     save_mode="reuse",
     batch_size=10000,
     verbose=True,
-    names=["trec_" + ("pruned_stopwords_and_special_tokens" if not args.no_pruning else "pruned_special_tokens")]
+    names=["trec_" + ("pruned_special_tokens" if not args.no_pruning else "no_pruning")]
 )
 
 del dense_e2e_bert_pruned
